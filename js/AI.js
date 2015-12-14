de83c74802ffa70f37ad28b56ffa79223300cc06
Feizhou <feizhoupeng@gmail.com>
2015-12-14 17:31:10 -0500

    Remove Redundancy

--- js/AI.js
+++ js/AI.js
@@ -73,7 +73,7 @@ function AIshow() {
 	
 	}
 	function islandSolve() {
-		console.log("Tank");
+		var mark = false;
 		var islands = b.numberOfIsland();
 		var res = new Array;
 		var min = 1000;
@@ -81,40 +81,16 @@ function AIshow() {
 			console.log("tank islands" );
 			console.log(islands[i]);
 			var cur = TankSolver(islands[i]);
-
 			res.push(cur);
 		}
-		// for (var i = 0; i < res.length; i++) {
-		// 	var cur = res[i];
-		// 	for (var j = 0; j < cur.length; j++) {
-		// 		if (cur[j] == 0) {
-		// 			min = 0;
-		// 			var num = islands[i][j];
-		// 			var col = Math.floor(num / num_row);
-		// 			var row = Math.floor(num % num_row);
-		// 			b.open_grid(col, row);
-		// 		}
-		// 	}
-		// }
-		// if (min == 0) {
-		// 	return;
-		// }
-		// console.log("calculate");
-		
 		var little = 1;
 		var big = 0;
 		var littleIndex = 0;
 		var bigIndex = 0;
-		var resIndex = new Array;
-		var mark = false;
 		for (var i = 0; i < islands.length; i++) {
 			var cur = islands[i];
-			var curRes = new Array;
-			var remain = b.getRemain();
-			recurseII(cur, b.getOpenBoundary(), curRes, 0, remain);
-			resIndex.push(curRes);
-			var solutionNum = curRes.length;
-			for (var j = 0; j < res[i].length; j++) {
+			var solutionNum = res[i][res[i].length - 1];
+			for (var j = 0; j < res[i].length - 1; j++) {
 				if (res[i][j] == 0) {
 					console.log("empty for sure");
 					var open = cur[j];
@@ -143,9 +119,7 @@ function AIshow() {
 				}
 			}
 		}
-		if (mark) {
-			return;
-		}
+		if (mark) return;
 		if (little <= 1 - big) {
 			var col = Math.floor(littleIndex / num_row);
 			var row = Math.floor(littleIndex % num_row);
@@ -160,78 +134,26 @@ function AIshow() {
 
 	}
 
-	function recurseII(unknown, boundary, result, k, remain) {
-		for (var i = 0; i < boundary.length; i++) {
-			var cur = boundary[i];
-			var col = Math.floor(cur / num_row);
-			var row = Math.floor(cur % num_row);
-			// too many mines
-			if (b.surroundingMarked(col, row, 2) > b.getCount(col, row)) {
-				return;
-			}
-			// too many empties
-			if (b.surroundingMarked(col, row, 0) + b.surroundingMarked(col, row, 2)
-				< b.getCount(col, row)) {
-				return;
-			}
-		}
-
-
-	
-		if (k == unknown.length) {
-			var total = 0;
-			for (var i = 0; i < unknown.length; i++) {
-				var cur = unknown[i];
-				var col = Math.floor(cur / num_row);
-				var row = Math.floor(cur % num_row);
-				if (b.getFlag(col, row) == 2) {
-					total++;
-				}
-			}
-			if (total > remain){
-				console.log(total + " " + remain);
-				console.log("invalid");
-				return;
-			} 
-			var res = new Array;
-			for (var i = 0; i < unknown.length; i++) {
-				var cur = unknown[i];
-				var col = Math.floor(cur / num_row);
-				var row = Math.floor(cur % num_row);
-				if (b.getFlag(col, row) == 2) {
-					res.push(cur);
-				}
-			}
-			result.push(res);
-			return;
-		}
-		var cur = unknown[k];
-		var col = Math.floor(cur / num_row);
-		var row = Math.floor(cur % num_row);
-		b.setFlag(col, row, 2);
-		recurse(unknown, boundary, result, k + 1, remain);
-		b.setFlag(col, row, 3);
-		recurse(unknown, boundary, result, k + 1, remain);
-		b.setFlag(col, row, 0);
-	}
-
-
-
-
-
 	function singleAreaSolve() {
 		var min = 1000;
 		var max = 0;
 		var unknown = b.getUnknownBoundary();
 		var stats = TankSolver(unknown);
-		for (var i =0; i < stats.length; i++) {
+		for (var i =0; i < stats.length - 1; i++) {
 			if (stats[i] == 0) {
 				min = 0;
 				var cur = unknown[i];
 				var col = Math.floor(cur / num_row);
 				var row = Math.floor(cur % num_row);
 				b.open_grid(col, row);
-			} else {
+			} else if (stats[i] == stats[stats.length - 1]) {
+				min = 0;
+				var cur = unknown[i];
+				var col = Math.floor(cur / num_row);
+				var row = Math.floor(cur % num_row);
+				b.mark_grid_AI(col, row);
+			} 
+			else {
 				min = Math.min(min, stats[i]);
 				max = Math.max(max, stats[i]);
 			}
@@ -247,13 +169,13 @@ function AIshow() {
 	function clickLeast(stats, min, unknown) {
 		// console.log(stats);
 		// console.log("dangerous: " + "min = " + min);
-		for (var i =0; i < stats.length; i++) {
+		for (var i =0; i < stats.length - 1; i++) {
 			if (stats[i] == min) {
 				var cur = unknown[i];
 				var col = Math.floor(cur / num_row);
 				var row = Math.floor(cur % num_row);
 				b.open_grid(col, row);
-				console.log("index is : " + col + " and " + row);
+				console.log("click index is : " + col + " and : " + row);
 				break;
 			}
 		}
@@ -261,31 +183,19 @@ function AIshow() {
 	function markLargest(stats, max, unknown) {
 		// console.log(stats);
 		// console.log("dangerous: " + "max = " + max);
-		for (var i =0; i < stats.length; i++) {
+		for (var i =0; i < stats.length - 1; i++) {
 			if (stats[i] == max) {
 				var cur = unknown[i];
 				var col = Math.floor(cur / num_row);
 				var row = Math.floor(cur % num_row);
 				b.mark_grid_AI(col, row);
-				console.log("index is : " + col + " and " + row);
+				console.log("mark index is : " + col + " and : " + row);
 				break;
 			}
 		}
 	}
 	function compareLeastLargest (stats, min, max, unknown) {
-
-		var minCount = 0;
-		var maxCount = 0;
-		for (var i = 0; i < stats.length; i++) {
-			var cur = stats[i];
-			if (cur == min) {
-				minCount++;
-			} 
-			if (cur == max) {
-				maxCount++;
-			}
-		}
-		if (minCount <= maxCount) {
+		if (min <= stats[stats.length - 1] - max) {
 			clickLeast(stats, min, unknown);
 		} else {
 			markLargest(stats, max, unknown);
@@ -299,8 +209,11 @@ function AIshow() {
 		for (var i = 0; i < unknown.length; i++) {
 			stats[i] = 0;
 		}
-
+		stats.push(0);
 		recurse(unknown, boundary, stats, 0, b.getRemain());
+		console.log(unknown);
+		console.log(stats);
+		console.log(stats.length - unknown.length);
 		return stats;
 	}
 
@@ -346,6 +259,7 @@ function AIshow() {
 					stats[i] = stats[i] + 1;
 				}
 			}
+			stats[stats.length - 1]++;
 			return;
 		}
 		var cur = unknown[k];
